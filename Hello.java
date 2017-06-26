c03a320ebbf0f7abeace99f0bec2daf2ac1f5577
avdn <avivey@163.com>
2017-06-26 18:39:41 +0800

    Change Hello.java and create Action.java For dev

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
+import java.util.*;
 public class Hello {
 	public static void main(String args [] ){
-		System.out.println("Hello Git !");
-		System.out.println("Hello Git1 !");
-        System.out.println("Hello Git2 !");
+		System.out.println("This is New print");
+		Date date = new Date();
 	}
 }
